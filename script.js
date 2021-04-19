30cbd9258cb7110771fb6240e46f89af9aac77dd
Roney-juma <ochirone10@gmail.com>
2021-04-19 13:21:07 +0300

    working on what we do section to collapse

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 $(Document).ready(function (){
+    $(".pic1").click(function(){
+        $(".pic1").hide(100)
+        $(".p1").show(100)
+    });
+    $(".p1").click(function(){
+        $(".p1").hide(100)
+        $(".pic1").show(100)
+    });
     
 })
+$(Document).ready(function (){
+    $(".pic2").click(function(){
+        $(".pic2").hide(100)
+        $(".p2").show(100)
+    });
+    $(".p2").click(function(){
+        $(".p2").hide(100)
+        $(".pic2").show(100)
+    });
+    
+})
+$(Document).ready(function (){
+    $(".pic3").click(function(){
+        $(".pic3").hide(100)
+        $(".p3").show(100)
+    });
+    $(".p3").click(function(){
+        $(".p3").hide(100)
+        $(".pic3").show(100)
+    });
+    
+})
+$(document).ready(function(){
+    $(".text").hover(function(){
+        $(this).animate({
+            opacity: '5'
+        })
+
+    })
+    function(){
+        $(this).animate({
+            opacity: '0'
+        })
+    }
+    
+})
+$('.collapse').collapse()
